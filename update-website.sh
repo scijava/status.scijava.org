e9ae88b13d80d427f44e916e1bd1d32042253b1e
Curtis Rueden <ctrueden@wisc.edu>
2017-10-29 21:07:42 -0500

    update-website.sh: fetch and update gh-pages
    Travis CI does not have the gh-pages branch initially.
    So we fetch it, then switch, then fast forward as needed.

--- update-website.sh
+++ update-website.sh
@@ -9,7 +9,9 @@
 # Regenerates the HTML content and pushes it to the gh-pages branch.
 
 ./html-report.sh > index-new.html &&
+git fetch &&
 git checkout gh-pages &&
+git merge --ff --ff-only 'HEAD@{u}' &&
 mv -f index-new.html index.html &&
 git add index.html &&
 git commit -m 'Update component table' &&
