8af2782d80d52cde0ccc00c263cd50b0ec1b75b1
Curtis Rueden <ctrueden@wisc.edu>
2017-10-31 12:00:52 -0500

    Fix the Travis build, hopefully
    This migrates the update-website.sh script into .travis/build.sh, since
    it is mostly Travis-specific now. However, if desired, it should still
    be possible to regenerate the table manually by running it locally.

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
-curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
-sh travis-build.sh
+
+# Regenerate the HTML content.
+./html-report.sh > index-new.html &&
+
+# Push it to the gh-pages branch.
+git clone --depth=1 --branch=gh-pages https://github.com/scijava/status.scijava.org site &&
+mv -f index-new.html site/index.html &&
+cd site &&
+git remote set-url origin --push git@github.com:scijava/status.scijava.org &&
+if [ "$TRAVIS_BUILD_NUMBER" ]
+then
+  commitNote="Travis build $TRAVIS_BUILD_NUMBER"
+else
+  commitNote=$(date)
+fi &&
+git commit -m "Update component table ($commitNote)" index.html &&
+git push
