d506fc9702b48f47b1904289e53ab5af97733937
Curtis Rueden <ctrueden@wisc.edu>
2017-10-31 22:11:36 -0500

    Travis: simply gh-pages cloning step

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,10 +4,9 @@
 ./html-report.sh > index-new.html &&
 
 # Push it to the gh-pages branch.
-git clone --depth=1 --branch=gh-pages https://github.com/scijava/status.scijava.org site &&
+git clone --depth=1 --branch=gh-pages git@github.com:scijava/status.scijava.org site &&
 mv -f index-new.html site/index.html &&
 cd site &&
-git remote set-url origin --push git@github.com:scijava/status.scijava.org &&
 if [ "$TRAVIS_BUILD_NUMBER" ]
 then
   commitNote="Travis build $TRAVIS_BUILD_NUMBER"
