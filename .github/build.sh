04753eb4c0f68f72b3fc67b4a911c5b797d6d5df
Mark Hiner <hinerm@gmail.com>
2021-07-02 13:14:45 -0500

    Try setting user/email inside script

--- .github/build.sh
+++ .github/build.sh
@@ -6,6 +6,8 @@
 # Push it to the gh-pages branch.
 git clone --depth=1 --branch=gh-pages git@github.com:scijava/status.scijava.org site &&
 mv -f index-new.html site/index.html &&
+git config --global user.name github-actions &&
+git config --global user.email github-actions@github.com &&
 cd site &&
 if git diff --quiet index.html
 then
