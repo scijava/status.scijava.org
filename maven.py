fc3df6e20a17d0c6d3f301c84ab5bb289426824b
Curtis Rueden <ctrueden@wisc.edu>
2021-10-27 17:06:23 -0500

    Combine maven and status modules
    The status module was Maven-side only, not GitHub or other sources.
    So let's just put the processing logic into the same module. KISS.

--- maven.py
+++ maven.py
@@ -8,15 +8,22 @@
 # ------------------------------------------------------------------------
 # Supporting library for harvesting metadata about Maven components.
 # Requires direct access to the backing storage of the repositories.
+#
+# When run from the command line, generates a data structure with information
+# about the components and repositories of the SciJava component collection.
 
-import datetime, logging, os, pathlib, re
+import datetime, json, logging, os, pathlib, re, sys
 import xml.etree.ElementTree as ET
+
+# -- Constants --
 
 storage = "/opt/sonatype-work/nexus/storage"
 release_repos = ["releases", "thirdparty", "sonatype", "central", "ome-releases"]
 snapshot_repos = ["snapshots", "sonatype-snapshots", "ome-snapshots"]
 
 ts_allowance = 10 # maximum seconds difference in SNAPSHOT timestamp
+
+# -- Classes --
 
 class XML:
 
@@ -191,3 +198,79 @@ class MavenComponent:
                 if os.path.exists(path):
                     return MavenPOM(path)
         return None
+
+# -- Functions --
+
+def resource_path(source):
+    return None if source is None else source[len(storage):]
+
+def status(c):
+    """
+    Gathers information from Maven about the given groupId:artifactId.
+    """
+    record = {
+        "groupId": c.groupId,
+        "artifactId": c.artifactId
+    }
+    record["release"] = None if c.release is None else {
+        "source": resource_path(c.release.source),
+        "groupId": c.release.groupId,
+        "artifactId": c.release.artifactId,
+        "lastUpdated": c.release.lastUpdated,
+        "latest": c.release.latest,
+        "lastVersion": c.release.lastVersion,
+        "release": c.release.release,
+    }
+    record["snapshot"] = None if c.snapshot is None else {
+        "source": resource_path(c.snapshot.source),
+        "groupId": c.snapshot.groupId,
+        "artifactId": c.snapshot.artifactId,
+        "lastUpdated": c.snapshot.lastUpdated,
+        "latest": c.snapshot.latest,
+        "lastVersion": c.snapshot.lastVersion,
+        "release": c.snapshot.release,
+    }
+    record["pom"] = None if c.pom is None else {
+        "source": resource_path(c.pom.source),
+        "groupId": c.pom.groupId,
+        "artifactId": c.pom.artifactId,
+        "version": c.pom.version,
+        "scm": c.pom.scmURL,
+        "issues": c.pom.issuesURL,
+        "ci": c.pom.ciURL,
+        "developers": c.pom.developers,
+    }
+    return record
+
+def matches(g, a, patterns):
+    return not patterns or any(re.match(pat, f"{g}:{a}") for pat in patterns)
+
+def process(patterns=[]):
+    g = "org.scijava"
+    a = "pom-scijava"
+    psj = MavenComponent(g, a)
+
+    if not psj.release and not psj.snapshot and not psj.pom:
+        return None
+
+    records = []
+
+    if matches(g, a, patterns):
+        records.append(status(psj))
+
+    for dep in psj.pom.elements("dependencyManagement/dependencies/dependency"):
+        g = dep.find("groupId").text
+        a = dep.find("artifactId").text
+
+        if matches(g, a, patterns):
+            c = MavenComponent(g, a)
+            records.append(status(c))
+
+    return records
+
+if __name__ == '__main__':
+    result = process(sys.argv[1:])
+    if result:
+        print(json.dumps(result, sort_keys=True, indent=4))
+    else:
+        print("This script must be run from the SciJava Maven server.")
